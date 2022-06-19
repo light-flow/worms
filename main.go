a70d56ea9c99397c8bd15469b20c4cc81fe820b9
lumen <1830712901@qq.com>
2022-06-19 17:08:30 +0800

    some changes

--- main.go
+++ main.go
@@ -8,4 +8,15 @@ import (
 func main() {
 	fmt.Println("main")
 	fmt.Println(utils.Add(4, 5))
+	var c string = "test"
+	var d int = 9
+	var f int8 = 10
+	f = int8(d)
+	c = c[1:3]
+	q := []int8{1, 23}
+	q[1] = 3
+	s := make([]int16, 2, 4)
+	p := new([]int16)
+	*p = append(*p, 3)
+	fmt.Println(c, d, f, q[0:2], &s, &p, len(*p))
 }
